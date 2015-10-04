82e122158de9cde4239ff50c4c8b40cdf68e0311
Yuta Funase <yfunase@gmail.com>
2015-10-04 11:02:27 +0000

    commit 10041802

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: {  maximum: 20 } , presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
+    # Age has to be integer and 1-3 letters
+    validates :age , length: {minimum:1, maximum: 3}, presence: true , numericality: {only_integer: true }
 end
