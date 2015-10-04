8111d3cc428d0a47a46ef7d84eff384febf90686
Yuta Funase <yfunase@gmail.com>
2015-10-04 10:05:37 +0000

    commit 10041705 g

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # acquire all the messages
     @messages = Message.all
     @message = Message.new
+  end
+  
+  def update
+    if @message.update(message_params)
+      # If successfully saved, redirect to top_page
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # If saving failed, go back to editiing page
+      render 'edit'
+    end
   end
   
   def create
@@ -20,6 +31,10 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
     
 end
